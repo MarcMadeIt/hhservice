c48f1aea6f5f22cbe6651409c84a05137fd11910
Marc Made It <spurdo.dk@gmail.com>
2025-02-11 00:18:26 +0100

    Update SetupUploadImages.tsx

--- src/components/admin/settings/setup/SetupUploadImages.tsx
+++ src/components/admin/settings/setup/SetupUploadImages.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import Image from "next/image";
 
 const SetupUploadImages = () => {
   const [file, setFile] = useState<File | null>(null);
@@ -37,7 +38,6 @@ const SetupUploadImages = () => {
       },
     });
 
-    const data = await res.json();
     if (res.ok) {
       setShowToast(true);
       setTimeout(() => setShowToast(false), 3000);
@@ -97,10 +97,11 @@ const SetupUploadImages = () => {
         <div className="md:flex-1">
           {preview && (
             <div className="relative w-64 h-36 overflow-hidden rounded-md border">
-              <img
+              <Image
                 src={preview}
                 alt="Preview"
-                className="absolute top-0 left-0 w-full h-full object-cover"
+                layout="fill"
+                objectFit="cover"
               />
             </div>
           )}
