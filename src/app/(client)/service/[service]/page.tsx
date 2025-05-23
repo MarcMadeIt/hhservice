973a076af1de0e3fdf7b84c1a62e3a346fc756a6
Marc Made It <spurdo.dk@gmail.com>
2025-05-23 14:58:29 +0200

    Update page.tsx

--- src/app/(client)/service/[service]/page.tsx
+++ src/app/(client)/service/[service]/page.tsx
@@ -1,17 +1,16 @@
 import { Metadata } from "next";
-import type { JSX } from "react";
 import ServiceClientWrapper from "./ServiceClientWrapper";
 import { generateMetadata as generateSeo } from "@/lib/client/seoData";
 
-type PageProps = { params: { service: string } };
-
 export async function generateMetadata({
   params,
-}: PageProps): Promise<Metadata> {
+}: {
+  params: { service: string };
+}): Promise<Metadata> {
   return generateSeo(params.service);
 }
 
-const Page = async (): Promise<JSX.Element> => {
+const Page = async () => {
   return <ServiceClientWrapper />;
 };
 
